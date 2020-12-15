1ff10c76d49f7ee83a133569a0e6f3ecc9483cdf
be-chan <70454487+be-chan@users.noreply.github.com>
2020-12-15 14:54:07 +0900

    with_optionsの追加
    制限内容をwith_optionsにまとめました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,15 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :password_confirmation, format: { with: VALID_PASSWORD_REGEX }
   # #全角(漢字、ひらがな、カタカナ)での入力が必須
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
   # #全角カタカナの入力が必須
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 
   validates :birthday, presence: true
 end
