54958dad9974f1d2418e4707db0040781e708d7f
be-chan <70454487+be-chan@users.noreply.github.com>
2021-01-11 22:41:01 +0900

    商品購入機能の不要なものの削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
   resources :items do
-    resources :purchase_recodes, only:[:index, :new, :create]
+    resources :purchase_recodes, only:[:index, :create]
   end
   resources :users, only:[:new, :create]
-  resources :addresses, only:[:index, :new, :create]
 end
